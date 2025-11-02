49c1743949b1b416cf8fd29eed7059d96e19dd2a
siva <sivakrishnaboya45@gmail.com>
2025-11-02 15:40:44 +0530

    add

---
+++ v1-springboot/cards/src/main/java/com/eazybytes/cards/exception/CardAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.eazybytes.cards.exception;
+
+public class CardAlreadyExistsException extends RuntimeException{
+	
+	public CardAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
