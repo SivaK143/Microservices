0241ed846552f2507e0a5f5a176643b6dcbb4055
siva <sivakrishnaboya45@gmail.com>
2025-11-29 17:33:17 +0530

    kafaka

---
+++ Kafka/accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -0,0 +1,27 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter@Setter@ToString@AllArgsConstructor@NoArgsConstructor
+public class Accounts extends BaseEntity{
+
+	 private Long customerId;
+
+	    @Id
+	    private Long accountNumber;
+
+	    private String accountType;
+
+	    private String branchAddress;
+	    
+	    @Column(name = "communication_sw")
+	    private Boolean communicationSw;
+}
